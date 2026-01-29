dbce2e2b1877a51b2c28a7bd9d79f93b352e2365
Gideon McKinlay <gmckinlay04@gmail.com>
2026-01-29 17:39:31 +1000

    Player head token backgrounds

--- src/main/java/golden/botc_mc/botc_mc/game/gui/PlayerHeadItemStack.java
+++ src/main/java/golden/botc_mc/botc_mc/game/gui/PlayerHeadItemStack.java
@@ -1,11 +1,17 @@
 package golden.botc_mc.botc_mc.game.gui;
 
+import golden.botc_mc.botc_mc.botc;
+import golden.botc_mc.botc_mc.game.seat.PlayerSeat;
 import golden.botc_mc.botc_mc.game.seat.Seat;
 import net.minecraft.component.DataComponentTypes;
+import net.minecraft.component.type.CustomModelDataComponent;
 import net.minecraft.component.type.ProfileComponent;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.util.Identifier;
+
+import java.util.List;
 
 /**
  * Utility class for creating player head ItemStacks.
@@ -28,12 +34,22 @@ public class PlayerHeadItemStack {
      * Create a player head ItemStack for the given seat.
      * If the seat has a player entity, use that player's head; otherwise, use a default player head.
      * The head will be named according to the seat's occupant text.
-     * Prefer using of(Seat, int) as it sets the seat number as the item count.
+     * Prefer using {@code of(Seat, int)} as it sets the seat number as the item count.
      * @param seat The seat whose occupant's head to create.
      * @return An ItemStack representing the seat's occupant's head.
      */
     public static ItemStack of(Seat seat) {
         ItemStack headItem = seat.hasPlayerEntity() ? of(seat.getPlayerEntity()) : new ItemStack(Items.PLAYER_HEAD);
+
+        headItem.set(DataComponentTypes.ITEM_MODEL, Identifier.of(botc.ID, "player_head"));
+
+        CustomModelDataComponent customModelDataComponent = new CustomModelDataComponent(
+            List.of(),
+            List.of(seat.isAlive(), seat instanceof PlayerSeat && ((PlayerSeat) seat).canGhostVote()),
+            List.of(), List.of()
+        );
+        headItem.set(DataComponentTypes.CUSTOM_MODEL_DATA, customModelDataComponent);
+
         headItem.set(DataComponentTypes.CUSTOM_NAME, seat.getOccupantText());
         return headItem;
     }
@@ -41,7 +57,7 @@ public class PlayerHeadItemStack {
     /**
      * Create a player head ItemStack for the given seat with a specific seat number.
      * The head will be named according to the seat's occupant text and the count set to the seat number.
-     * This is preferred over of(Seat) as it sets the seat number as the item count.
+     * This is preferred over {@code of(Seat)} as it sets the seat number as the item count.
      * @param seat The seat whose occupant's head to create.
      * @param seatNumber The seat number to set as the item count.
      * @return An ItemStack representing the seat's occupant's head with the specified count.
