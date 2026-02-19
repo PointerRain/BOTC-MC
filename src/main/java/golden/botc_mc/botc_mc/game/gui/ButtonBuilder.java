a22b9b3b28ff3b8acaa064fc6ff303788ad4168e
Gideon McKinlay <gmckinlay04@gmail.com>
2026-02-19 14:12:33 +1000

    Clear formatting on buttons

--- src/main/java/golden/botc_mc/botc_mc/game/gui/ButtonBuilder.java
+++ src/main/java/golden/botc_mc/botc_mc/game/gui/ButtonBuilder.java
@@ -1,23 +1,26 @@
 package golden.botc_mc.botc_mc.game.gui;
 
 import eu.pb4.sgui.api.elements.GuiElement;
-import golden.botc_mc.botc_mc.botc;
 import net.minecraft.component.DataComponentTypes;
-import net.minecraft.component.type.BundleContentsComponent;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
+import net.minecraft.text.Style;
 import net.minecraft.text.Text;
-import net.minecraft.util.Identifier;
+import net.minecraft.util.Formatting;
 
-import java.util.List;
-
+/**
+ * Utility class for building button GuiElements with consistent styling and behaviour.
+ */
 public class ButtonBuilder {
     /**
      * Build a simple button GuiElement with a paper icon and custom name and click callback.
+     * Prefer using {@link #buildButton(Text, ButtonIcon, GuiElement.ClickCallback)}
+     * or {@link #buildButton(Text, ItemStack, GuiElement.ClickCallback)} with a button icon.
      * @param name     The display name of the button.
      * @param callback The click callback for the button.
      * @return The constructed GuiElement button.
      */
+    @SuppressWarnings("unused") // Suppress unused warnings for this method, as it is convenient default button builder.
     public static GuiElement buildButton(Text name, GuiElement.ClickCallback callback) {
         return buildButton(name, new ItemStack(Items.PAPER), callback);
     }
@@ -42,6 +45,7 @@ public class ButtonBuilder {
      */
     public static GuiElement buildButton(Text name, ItemStack icon, GuiElement.ClickCallback callback) {
         ItemStack itemButton = icon.copy();
+        name = name.copy().setStyle(Style.EMPTY.withColor(Formatting.WHITE).withItalic(false));
         itemButton.set(DataComponentTypes.CUSTOM_NAME, name);
         return new GuiElement(itemButton, callback);
     }
