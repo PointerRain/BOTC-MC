e1dbf9908203e258be4786d329a490bc0f1796dc
Gideon McKinlay <gmckinlay04@gmail.com>
2026-04-18 16:18:50 +1000

    Bundle ItemStack generator

--- src/main/java/golden/botc_mc/botc_mc/game/gui/ButtonIcon.java
+++ src/main/java/golden/botc_mc/botc_mc/game/gui/ButtonIcon.java
@@ -1,9 +1,5 @@
 package golden.botc_mc.botc_mc.game.gui;
 
-import golden.botc_mc.botc_mc.game.Script;
-import net.minecraft.component.DataComponentTypes;
-import net.minecraft.component.type.DyedColorComponent;
-import net.minecraft.item.BundleItem;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.util.DyeColor;
@@ -63,38 +59,9 @@ public enum ButtonIcon {
             case MORE -> new ItemStack(Items.PAPER);
             case LESS -> new ItemStack(Items.PAPER);
 
-            case BAG -> {
-                ItemStack item = new ItemStack(Items.BUNDLE);
-                item.remove(DataComponentTypes.BUNDLE_CONTENTS);
-                yield item;
-            }
+            case BAG -> BundleItemStack.of((DyeColor) null);
 
             default -> new ItemStack(Items.PAPER); // Fallback
         };
-    }
-
-    public static ItemStack bagIcon(DyeColor color) {
-        ItemStack item = new ItemStack(
-                (color != null) ? BundleItem.getBundle(color) : Items.BUNDLE
-        );
-        item.remove(DataComponentTypes.BUNDLE_CONTENTS);
-        return item;
-    }
-
-    public static ItemStack bagIcon(Script script) {
-        DyeColor color = switch (script.meta().name()) {
-            // Matching on name is very yucky but missing shouldn't have consequences
-            case "Trouble Brewing" -> DyeColor.RED;
-            case "Sects & Violets" -> DyeColor.PURPLE;
-            case "Bad Moon Rising" -> DyeColor.ORANGE;
-            case "Garden of Sin" -> DyeColor.GREEN;
-            case "Midnight in the House of the Damned" -> DyeColor.BLUE;
-            default -> null;
-        };
-        ItemStack item = bagIcon(color);
-        if (script.hasColour()) {
-            item.set(DataComponentTypes.DYED_COLOR, new DyedColorComponent(script.colourInt()));
-        }
-        return item;
     }
 }
